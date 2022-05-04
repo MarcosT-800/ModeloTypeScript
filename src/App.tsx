9de141032046d143c193848d215e0efed3439255
Marcos Alves <82832426+MarcosT-800@users.noreply.github.com>
2022-05-04 17:14:03 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Page from '../src/components/Page'
+import { GlobalStyles } from './Styled/Global'
+
+function App() {
+  return (
+   <>
+   <Page />
+   <GlobalStyles />
+   </>
+  );
+}
+
+export default App;
---
+++ src/Styled/Global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+
+* {
+    background: black;
+    color: white;
+    margin: 0 ;
+    padding: 0;
+}
+ `
